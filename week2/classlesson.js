00e6f7d6c2bbe539c0a06b3b45481ae775f1b8d1
hblanco <harvey@lifetimebikes.com>
2022-02-09 19:41:46 -0600

    magic8

---
+++ week2/classlesson.js
@@ -0,0 +1,9 @@
+let question = prompt('What is your question?');
+
+if(question.includes('love')){
+    alert('Nope!');
+}else if(question.includes('hate')){
+    alert('Yeah');
+}else {
+    alert('Maybe');
+}
