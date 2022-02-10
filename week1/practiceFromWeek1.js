7ebd298d8b1f52de352bec16956b0badb34e016d
hblanco <harvey@lifetimebikes.com>
2022-02-09 18:43:35 -0600

    NewPracticeWeek1

---
+++ week1/practiceFromWeek1.js
@@ -0,0 +1,31 @@
+var cupsOfFlour = 0;
+
+console.log('Scooping a cup of flour into the bowl');
+
+cupsOfFlour += 1;
+
+console.log('There are ' + cupsOfFlour + ' cups of flour in the bowl');
+
+console.log('Scooping a cup of flour into the bowl');
+
+cupsOfFlour += 1;
+
+console.log('There are ' + cupsOfFlour + ' cups of flour in the bowl');
+
+console.log('Scooping a cup of flour into the bowl');
+
+cupsOfFlour += 1;
+
+console.log('There are ' + cupsOfFlour + ' cups of flour in the bowl');
+
+console.log('Scooping a cup of flour into the bowl');
+
+cupsOfFlour += 1;
+
+console.log('There are ' + cupsOfFlour + ' cups of flour in the bowl');
+
+console.log('Scooping a cup of flour into the bowl');
+
+cupsOfFlour += 1;
+
+console.log('There are ' + cupsOfFlour + ' cups of flour in the bowl');
