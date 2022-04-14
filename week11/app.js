8644d9e99075e0d9345fda35a7d1985524bd3023
hblanco <harvey@lifetimebikes.com>
2022-04-14 14:27:07 -0500

    week11Research

--- week11/app.js
+++ week11/app.js
@@ -22,3 +22,12 @@ ul.remove();
 
 $('input').hide();
 setTimeout(() => $('input').show(), 2000)
+
+
+
+$.get('https://reqres.in/api/users/2', (data) => console.log(data));
+
+$.post('https://reqres.in/api/users', {
+    name: 'Tommy',
+    job: 'Front End Software Developer'
+}, (data) => console.log(data));
