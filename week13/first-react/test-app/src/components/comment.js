e77e3a3658eb60d3fa1b48df5bc6440aade6ea4b
hblanco22 <harvey@lifetimebikes.com>
2022-04-27 19:10:44 -0500

    reactWeek13

---
+++ week13/first-react/test-app/src/components/comment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReplyButton from './reply-button';
+import LikeButton from './like-button';
+
+let e = React.createElement;
+
+export default class Comment extends React.Component {
+    render() {
+        return (
+            <div className='card w-75'>
+                <div className='card-header bg-success text-white'>
+                    Username and Time
+                </div>
+                <div className='card-body'>
+                    Comment Content
+                </div>
+                <div className='card-footer'>
+                    <LikeButton />
+                    <ReplyButton />
+
+                </div>
+            </div>
+        );
+    }
+}
