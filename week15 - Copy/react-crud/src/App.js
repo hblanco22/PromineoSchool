4784ca1f571ecdbfdf923f2073def0f000cfd67a
hblanco22 <harvey@lifetimebikes.com>
2022-05-27 17:50:02 -0500

    recoveredAppAfterfail

--- week15 - Copy/react-crud/src/App.js
+++ week15 - Copy/react-crud/src/App.js
@@ -3,13 +3,19 @@ import Create from './components/create';
 import Read from './components/read';
 import Update from './components/update';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/header/header';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <Router>
+      <Header />
       <div className="main">
-        <h2 className="main-header">React Crud Operations</h2>
+        <h1 className="bold">Acme Inc. Human Resources Employee Info</h1>
+        <br></br>
+        <br></br>
         <div>
+
           <Route exact path='/' component={Create} ></Route>
           
         </div>
